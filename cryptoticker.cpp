f62d40afe70849a3aba0126388f182cf8fe87a75
Alexandru Carusu <41578311+Aclaerxius@users.noreply.github.com>
2024-03-07 17:34:17 +0200

    Added all files related to the project

---
+++ cryptoticker.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <sstream>
+#include <iostream>
+#include <json.hpp>
+
+using namespace std;
+
+class UserConfig
+{
+
+}
+
+class BiannceDataModel
+{
+
+}
+
+class RequestManager
+{
+
+}
+
+class OutputModel
+{
+
+}
+
+class OutputComputationManager
+{
+
+}
+
+class DisplayManager
+{
+
+}
+
+int
+main(int argc, char *argv[])
+{
+
+    return 0;
+}
