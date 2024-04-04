45fff1436876da2793030bd0056fb38287ac8d9e
Alexandru Carusu <41578311+Aclaerxius@users.noreply.github.com>
2024-04-04 18:50:25 +0300

    Project setup (#1)
    * Added semicolons to end of each class
    
    * Latest structure updates
    
    * added gitignore
    
    * removed executable
    
    * project formatting
    
    * Project setup
    
    ---------
    
    Co-authored-by: skytz <sky_razvan@yahoo.com>

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "test.h"
+#include "include/json.hpp"
+#include "include/color.hpp"
+
+using namespace std;
+
+int main()
+{
+  int v = 5;
+
+  int x = 10;
+  std::cout << "Hello,---,,, , World2!" << TEST_DEFINE << v + x << std::endl;
+  return 0;
+}
