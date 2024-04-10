c980e67bd10e8c3040ef45ade4ad104e917f876e
Aclaerxius <alicarusu@gmail.com>
2024-04-10 16:50:03 +0300

    print and parse functions calle in main

--- main.cpp
+++ main.cpp
@@ -1,39 +1,21 @@
-#include <iostream>
 #include <stdlib.h>
-#include "include/color.hpp"
-#include "include/VariadicTable.h"
-#include "include/json.hpp"
+
+#include <iostream>
+
 #include "include/HTTPRequest.hpp"
+#include "include/VariadicTable.h"
+#include "include/color.hpp"
+#include "include/json.hpp"
 #include "test.h"
 #include "user_input.h"
 
 using namespace std;
 
-vector<string> valid_sort_columns = {"symbol", "price", "price_change", "volume", "number_of_trades", "trend"};
+// vector<string> valid_sort_columns = {"symbol", "price", "price_change", "volume", "number_of_trades", "trend"};
 
-int main(int argc, char *argv[])
-{
-  for (const auto &sortInput : userInput.sort)
-  {
-    cout << "Column: " << sortInput.column << ", Order: " << sortInput.order << endl;
-  }
-  cout << "Limit: " << userInput.limit << endl;
-  for (const auto &pairsInput : userInput.pairs)
-  {
-    cout << "Pair: " << pairsInput << endl;
-  }
-  cout << "Ignore Errors: " << userInput.ignoreErrors << endl;
-  cout << "Live: " << userInput.live << endl;
-  cout << "Trend: " << userInput.trend << endl;
-  // // Print the sub-vectors to verify the result
-  // for (const auto &subVector : userArgsSubVectors)
-  // {
-  //   for (const auto &option : subVector)
-  //   {
-  //     cout << option << " ";
-  //   }
-  //   cout << endl;
-  // }
+int main(int argc, char *argv[]) {
+    UserInput userInput = UserInput::parse(argc, argv);
+    userInput.print();
 
-  return 0;
+    return 0;
 }
--- user_input.h
+++ user_input.h
@@ -1,21 +1,19 @@
 #pragma once
 #define USERINPUTCLASS
-
-#include <iostream>
 #include <stdlib.h>
 
-using namespace std;
+#include <iostream>
+#include <string>
+#include <vector>
 
-class SortInput
-{
-public:
+class SortInput {
+   public:
     string column;
     string order;
 };
 
-class UserInput
-{
-public:
+class UserInput {
+   public:
     std::vector<SortInput> sort;
     int limit;
     vector<string> pairs;
@@ -23,96 +21,76 @@ public:
     int live;
     bool trend;
 
-    UserInput parse(int argc, char *argv[])
-    {
-        void print_help();
+    static UserInput parse(int argc, char* argv[]) {
+        UserInput userInput;
 
         // store user input arguments in a vector string
         vector<string> userArgs;
-        for (int i = 0; i < argc; i++)
-        {
+        for (int i = 0; i < argc; i++) {
             string s(argv[i]);
             userArgs.push_back(s);
         };
 
         vector<vector<string>> userArgsSubVectors;
 
-        for (int i = 1; i < userArgs.size(); i++)
-        {
+        for (int i = 1; i < userArgs.size(); i++) {
             string arg = userArgs.at(i);
 
-            if (arg.length() > 1 && arg.at(0) == '-' && arg.at(1) == '-')
-            {
+            if (arg.length() > 1 && arg.at(0) == '-' && arg.at(1) == '-') {
                 userArgsSubVectors.push_back(std::vector<std::string>());
                 userArgsSubVectors.back().push_back(arg);
-            }
-            else
-            {
-                if (!userArgsSubVectors.empty())
-                {
+            } else {
+                if (!userArgsSubVectors.empty()) {
                     userArgsSubVectors.back().push_back(arg);
                 }
             }
         }
 
-        UserInput userInput;
-        for (const auto &subVector : userArgsSubVectors)
-        {
+        for (const auto& subVector : userArgsSubVectors) {
             string sub = subVector[0];
             // Sort
 
-            if (sub == "--sort" && subVector.size() > 1 && subVector.size() <= 12)
-            {
-                for (int i = 1; i < subVector.size(); i++)
-                {
+            if (sub == "--sort" && subVector.size() > 1 && subVector.size() <= 12) {
+                for (int i = 1; i < subVector.size(); i++) {
                     string sub1 = subVector[i];
                     string sub2 = subVector[i + 1];
 
                     SortInput newSortInput;
                     newSortInput.column = sub1;
 
-                    if (sub1 == "symbol")
-                    {
+                    if (sub1 == "symbol") {
                         newSortInput.column = "symbol";
                     }
 
-                    if (sub1 == "price")
-                    {
+                    if (sub1 == "price") {
                         newSortInput.column = "price";
                     }
 
-                    if (sub1 == "price_change")
-                    {
+                    if (sub1 == "price_change") {
                         newSortInput.column = "price_change";
                     }
 
-                    if (sub1 == "volume")
-                    {
+                    if (sub1 == "volume") {
                         newSortInput.column = "volume";
                     }
 
-                    if (sub1 == "number_of_trades")
-                    {
+                    if (sub1 == "number_of_trades") {
                         newSortInput.column = "number_of_trades";
                     }
 
-                    if (sub1 == "trend")
-                    {
+                    if (sub1 == "trend") {
                         newSortInput.column = "trend";
                     }
 
-                    if (sub2 == "asc")
-                    {
+                    if (sub2 == "asc") {
                         newSortInput.order = "asc";
                     }
 
-                    if (sub2 == "desc")
-                    {
+                    if (sub2 == "desc") {
                         newSortInput.order = "desc";
                     }
 
-                    if (!newSortInput.column.empty() && !newSortInput.order.empty())
-                    {
+                    if (!newSortInput.column.empty() && !newSortInput.order.empty()) {
                         userInput.sort.push_back(newSortInput);
                     }
                     // if (sub1 != "symbol" && sub1 != "price" && sub1 != "price_change" && sub1 != "volume" && sub1 != "number_of_trades" && sub1 != "trend" && sub2 != "asc" && sub2 != "desc")
@@ -123,22 +101,17 @@ public:
             }
 
             // Limit
-            if (sub == "--limit")
-            {
-                for (int i = 1; i < subVector.size(); i++)
-                {
-                    if (i > 0)
-                    {
+            if (sub == "--limit") {
+                for (int i = 1; i < subVector.size(); i++) {
+                    if (i > 0) {
                         userInput.limit = stoi(subVector[i]);
                     }
                 }
             }
 
             // Pairs
-            if (sub == "--pairs")
-            {
-                for (int i = 1; i < subVector.size(); i++)
-                {
+            if (sub == "--pairs") {
+                for (int i = 1; i < subVector.size(); i++) {
                     string newPair;
                     newPair = subVector[i];
                     userInput.pairs.push_back(newPair);
@@ -146,47 +119,48 @@ public:
             }
 
             // Ignore errors
-            if (sub != "--ignore-errors")
-            {
+            if (sub != "--ignore-errors") {
                 userInput.ignoreErrors = false;
-            }
-            else
-            {
+            } else {
                 userInput.ignoreErrors = true;
             }
 
             // Live
-            if (sub != "--live")
-            {
+            if (sub != "--live") {
                 userInput.live = 0;
-            }
-            else
-            {
+            } else {
                 userInput.live = 5;
-                for (int i = 1; i < subVector.size(); i++)
-                {
-                    if (i > 0 && i < 100)
-                    {
+                for (int i = 1; i < subVector.size(); i++) {
+                    if (i > 0 && i < 100) {
                         userInput.live = stoi(subVector[i]);
-                    }
-                    else if (i > 100)
-                    {
+                    } else if (i > 100) {
                         userInput.live = 100;
                     }
                 }
             }
 
             // Trend
-            if (sub != "--trend")
-            {
+            if (sub != "--trend") {
                 userInput.trend = false;
-            }
-            else
-            {
+            } else {
                 userInput.trend = true;
             }
         }
 
         return userInput;
     };
+
+    void print();
+};
+void UserInput::print() {
+    for (const auto& sortInput : sort) {
+        std::cout << "Column: " << sortInput.column << ", Order: " << sortInput.order << std::endl;
+    }
+    std::cout << "Limit: " << limit << std::endl;
+    for (const auto& pairsInput : pairs) {
+        std::cout << "Pair: " << pairsInput << std::endl;
+    }
+    std::cout << "Ignore Errors: " << ignoreErrors << std::endl;
+    std::cout << "Live: " << live << std::endl;
+    std::cout << "Trend: " << trend << std::endl;
 };
