2a8b356a7cf9456c40d3655317ee050d95409f6f
Aclaerxius <alicarusu@gmail.com>
2024-04-16 20:43:51 +0300

    HTTP Request done

--- main.cpp
+++ main.cpp
@@ -12,23 +12,11 @@ using namespace std;
 
 // vector<string> valid_sort_columns = {"symbol", "price", "price_change", "volume", "number_of_trades", "trend"};
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     UserInput userInput = UserInput::parse(argc, argv);
     userInput.print();
 
-    RequestManager requestManager;
-    vector<BianceDataModel> dataModels = requestManager.get_data();
-
-    for (const auto &model : dataModels)
-    {
-        std::cout << "Symbol: " << model.symbol << std::endl;
-        std::cout << "Price: " << model.price << std::endl;
-        std::cout << "Price Change: " << model.priceChange << std::endl;
-        std::cout << "Volume: " << model.volume << std::endl;
-        std::cout << "Number of Trades: " << model.numberOfTrades << std::endl;
-        std::cout << "-------------------------" << std::endl;
-    }
+    print_data();
 
     return 0;
 }
--- request_manager.h
+++ request_manager.h
@@ -9,67 +9,78 @@
 using namespace std;
 using json = nlohmann::json;
 
-class BianceDataModel
-{
-public:
+class BianceDataModel {
+   public:
     string symbol;
     double price;
     double priceChange;
     double volume;
     int numberOfTrades;
 
-    static BianceDataModel parse(const json &json_object)
-    {
+    static BianceDataModel parse(const json &json_object) {
         BianceDataModel model;
 
-        try
-        {
+        try {
             model.symbol = json_object.at("symbol").get<string>();
-            model.price = json_object.at("lastPrice").get<double>();
-            model.priceChange = json_object.at("priceChangePercent").get<double>();
-            model.volume = json_object.at("quoteVolume").get<int>();
-            model.numberOfTrades = json_object.at("lastId").get<double>();
-        }
-        catch (const json::parse_error &e)
-        {
+            model.price = stod(json_object.at("lastPrice").get<string>());
+            model.priceChange = stod(json_object.at("priceChangePercent").get<string>());
+            model.volume = stod(json_object.at("quoteVolume").get<string>());
+            model.numberOfTrades = json_object.at("lastId").get<int>();
+        } catch (const json::parse_error &e) {
             cerr << "Error parsing JSON: " << e.what() << endl;
         }
         return model;
     }
 };
 
-class RequestManager
-{
-private:
+class RequestManager {
+   private:
     string url = "https://api2.binance.com/api/v3/ticker/24hr";
 
-public:
-    RequestManager getUrl(const string &url)
-    {
+   public:
+    RequestManager getUrl(const string &url) {
         this->url = url;
         return *this;
     };
 
-    vector<BianceDataModel> get_data()
-    {
-        vector<BianceDataModel> binance_models;
+    json request() {
         json jsonResponse;
-        try
-        {
+
+        try {
             httplib::Client cli{"https://api2.binance.com"};
             const auto response = cli.Get("/api/v3/ticker/24hr");
             jsonResponse = json::parse(response->body);
             cout << string{response->body.begin(), response->body.end()} << '\n';
-        }
-        catch (const exception &e)
-        {
+        } catch (const exception &e) {
             cerr << "Request failed, error: " << e.what() << '\n';
         }
-        for (const auto &json_object : jsonResponse)
-        {
+        return jsonResponse;
+    }
+
+    vector<BianceDataModel> response(const json &jsonResponse) {
+        vector<BianceDataModel> binance_models;
+        for (const auto &json_object : jsonResponse) {
             auto binance_model = BianceDataModel::parse(json_object);
             binance_models.push_back(binance_model);
         }
         return binance_models;
     }
+
+    vector<BianceDataModel> get_data() {
+        json jsonResponse = request();
+        return response(jsonResponse);
+    }
 };
+
+void print_data() {
+    RequestManager requestManager;
+    vector<BianceDataModel> dataModels = requestManager.get_data();
+    for (const auto &model : dataModels) {
+        std::cout << "Symbol: " << model.symbol << std::endl;
+        std::cout << "Price: " << model.price << std::endl;
+        std::cout << "Price Change: " << model.priceChange << std::endl;
+        std::cout << "Volume: " << model.volume << std::endl;
+        std::cout << "Number of Trades: " << model.numberOfTrades << std::endl;
+        std::cout << "-------------------------" << std::endl;
+    }
+}
